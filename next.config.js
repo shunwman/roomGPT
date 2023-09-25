6bab39d3c499264ada5bcf038ad7fd159a9a0615
Joedd <manshunwing@gmail.com>
2023-09-25 18:08:02 +0800

    fixed the image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbxt.replicate.delivery',
+      },
+    ],
+    domains: ["upcdn.io", "replicate.delivery","pbxt.replicate.delivery"],
   },
 };
